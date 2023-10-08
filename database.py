83c60d817ae5bb1d0e245ea74f0db2aaeb2edb39
jjtan444 <46512542+jjtan444@users.noreply.github.com>
2023-10-08 10:38:10 +0000

    removed pwd

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = "mysql+pymysql://m3xajd60w5m7g03d41xh:pscale_pw_15eeKIS2eomu8Ohvj52vJmAbLpONZiz8oFy5P2ErseN@aws.connect.psdb.cloud/cloud?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
